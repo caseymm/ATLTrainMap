a8296592f5e1685c1b10369e0a2bfdf642c225c0
caseymm <caseymm@gmail.cpom>
2013-07-16 16:49:37 -0400

    before graph

--- js/geo.js
+++ js/geo.js
@@ -1,12 +1,12 @@
 var crossingData = {"type":"FeatureCollection","features":[
-{"type":"Feature","id":"1","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-104.99404191970824, 39.756213909328125]}},
-{"type":"Feature","id":"2","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 39.756213909328125]}},
-{"type":"Feature","id":"3","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-106.99404191970824, 39.756213909328125]}},
-{"type":"Feature","id":"4","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-107.99404191970824, 39.756213909328125]}},
-{"type":"Feature","id":"5","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-108.99404191970824, 39.756213909328125]}},
-{"type":"Feature","id":"6","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 40.756213909328125]}},
-{"type":"Feature","id":"7","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 41.756213909328125]}},
-{"type":"Feature","id":"8","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 42.756213909328125]}},
-{"type":"Feature","id":"9","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 43.756213909328125]}},
-{"type":"Feature","id":"10","properties":{"name":"District of Columbia","percentage":0.10},"geometry":{"type":"Point","coordinates":[-105.99404191970824, 44.756213909328125]}}
+{"type":"Feature","id":"1","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.761381]}},
+{"type":"Feature","id":"2","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.493346, 33.761381]}},
+{"type":"Feature","id":"3","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.593346, 33.761381]}},
+{"type":"Feature","id":"4","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.693346, 33.761381]}},
+{"type":"Feature","id":"5","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.793346, 33.761381]}},
+{"type":"Feature","id":"6","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.861381]}},
+{"type":"Feature","id":"7","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.961381]}},
+{"type":"Feature","id":"8","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.361381]}},
+{"type":"Feature","id":"9","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.461381]}},
+{"type":"Feature","id":"10","properties":{"name":"Rail Crossing/Intersection","number":34, "incidents":"incident 1 <br />incident 2 <br />incident 3"},"geometry":{"type":"Point","coordinates":[-84.393346, 33.561381]}}
 ]};
